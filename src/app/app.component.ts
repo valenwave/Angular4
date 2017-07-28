744a7e179b2548d4dc1e876a4f193825e3562a09
Phi Dao <phi.dao@cuanswers.com>
2017-07-28 14:32:32 -0400

    2-Way Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'the test application';
   secret = '42';
+  monster = 'Beast';
+  story = {
+    name: 'The strike'
+  };
+  tasty = {
+    name: 'Cat',
+    value: 7
+  };
+  vehicles = ['cat', 'bat', 'tact']
 }
 
+// export class StoryController {
+//   var vm = this;
+//   vm.story = { id: 100, name: 'The Force' };
+// }
---
+++ src/app/sauce/sauce.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sauce',
+  templateUrl: './sauce.component.html',
+  styleUrls: ['./sauce.component.css']
+})
+export class SauceComponent {
+  cake = 'yes please';
+}
